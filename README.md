7d99fb5d5e3995fabae1096184695af73f3d58f6
Benjamin Funayama <228306427+bfun01@users.noreply.github.com>
2025-12-02 12:35:29 +1100

    BUILD: Implement playGame function to run game
    playGame function calls playRound 5x and outputs a message at the end
    declaring the winner. Moved variable declarations to inside
    the PlayRound function.
    
    Added round variable to keep track of rounds.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 TOP Rock Paper Scissors JS project
 
 This project is part of the TOP curriculum and is a simple rock, paper, scissors game.
-The project will be played in the console (part 1) and is coded in JS. A user should input "rock", "paper", or "scissors". The code will also randomly pick one of the three choices. The game will message whether the use won, lost, or drew. 
+The project will be played in the console (part 1) and is coded in JS. A user should input "rock", "paper", or "scissors". The code will also randomly pick one of the three choices. The game will message whether the use won, lost, or drew.
--- script.js
+++ script.js
@@ -8,9 +8,10 @@
 //      4b. Make humanChoice parameter case-insensitive
 //      4b. Check which is the winner, log message into console
 //      4c. Increment human or computer score based on winner
+// 5. Call 5 rounds of the game. Have a message at the end declaring the winner.
 
-
-// Step 1: Computer Choice
+let round = 1
+// Computer Choice
 let compchoice //Pass function result to a variable so I can call the result later.
 
 function getComputerChoice() {
@@ -21,61 +22,73 @@ function getComputerChoice() {
     } else {
         compchoice = "scissors"; 
     }
-} 
+}
     
-getComputerChoice();
-
-console.log("Computer chooses: "+compchoice);
-    
-// Step 2: Human Choice
-
+// Human Choice
 let choice //Pass function result to a variable so I can call the result later.
 
 function getHumanChoice() {
-    choice = prompt("Rock, paper or scissors?");
+    choice = prompt("Round "+round+"/5"+": Rock, paper or scissors?");
 }
 
-getHumanChoice();
-console.log("You chose: " +choice);
-
-// Step 3: Variables to keep track of score
-
-let humanScore = 0;
+// Round logic
+let humanScore = 0; // Variables to keep track of score
 let computerScore = 0;
 
-// Step 4: Round logic
+function playRound() {
 
-function playRound(humanChoice, computerChoice) {
+    getComputerChoice();
+    console.log("Computer chooses: "+compchoice);
+
+    getHumanChoice();
+    console.log("You chose: " +choice);
+
     humanChoice = choice.toLowerCase(); //If getHumanChoice function is called here, it reruns the prompt, hence passing the result to the "choice" variable instead
     computerChoice = compchoice; //If getComputerChoice function is called here, it rerolls the result. hence passing the result to "compchoice" variable
+   
     if (humanChoice === computerChoice) {
-        console.log("It's a draw!!!");
+        console.log("Round "+round+": It's a draw!!!");
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
         humanScore++;
-        console.log("You are the winner!!!")
+        console.log("Round "+round+": You are the winner!!!")
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         humanScore++;
-        console.log("You are the winner!!!")
+        console.log("Round "+round+": You are the winner!!!")
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         humanScore++;
-        console.log("You are the winner!!!")
+        console.log("Round "+round+": You are the winner!!!")
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         computerScore++;
-        console.log("You lose, loser!!!")
+        console.log("Round "+round+": You lose, loser!!!")
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         computerScore++;
-        console.log("You lose, loser!!!")
+        console.log("Round "+round+": You lose, loser!!!")
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         computerScore++;
-        console.log("You lose, loser!!!")
+        console.log("Round "+round+": You lose, loser!!!")
     } else { 
         console.log("Oh god, something went wrong - do you know how to spell?")
     }
+    console.log("You: "+humanScore+" Computer: "+computerScore);
+    round++;
 }
 
-playRound();
-console.log("You: "+humanScore+" Computer: "+computerScore);
+// Step 5: Oh god here we go
 
-// Step 5: Oh god her we go
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    if (humanScore > computerScore) {
+        console.log("FINAL SCORE - "+"You: "+humanScore+" Computer: "+computerScore+" YOU WIN!");
+    } else if (humanScore < computerScore) {
+        console.log("FINAL SCORE - "+"You: "+humanScore+" Computer: "+computerScore+" YOU LOSE!");
+    } else {
+        console.log("FINAL SCORE - "+"You: "+humanScore+" Computer: "+computerScore+" It was a draw. How boring")
+    }
+}
 
 playGame();
